d4025b3210b6a94222853e8963b4355adadb09aa
Caroline Bogart <caroline.bogart@gmail.com>
2026-02-20 21:27:48 -0500

    hostinger express

---
+++ sharp-test/server.js
@@ -0,0 +1,17 @@
+const { createServer } = require("http");
+const { parse } = require("url");
+const next = require("next");
+
+const port = parseInt(process.env.PORT, 10) || 3000;
+const dev = false;
+const app = next({ dev });
+const handle = app.getRequestHandler();
+
+app.prepare().then(() => {
+  createServer((req, res) => {
+    const parsedUrl = parse(req.url, true);
+    handle(req, res, parsedUrl);
+  }).listen(port, "0.0.0.0", () => {
+    console.log(`> Ready on http://0.0.0.0:${port}`);
+  });
+});
